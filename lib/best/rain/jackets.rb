a1c100580fc173a5c4cb923a5af5f31dafbd971c
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-24 21:56:33 +0000

    first commit

---
+++ lib/best/rain/jackets.rb
@@ -0,0 +1,10 @@
+require "best/rain/jackets/version"
+
+module Best
+  module Rain
+    module Jackets
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/best/rain/jackets/version.rb
@@ -0,0 +1,7 @@
+module Best
+  module Rain
+    module Jackets
+      VERSION = "0.1.0"
+    end
+  end
+end
